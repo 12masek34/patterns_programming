925b00fd761d6346a50e2ea8c7031fea08aa1533
dmitriy_martys <12masek34@mail.ru>
2022-05-25 08:05:28 +0300

    Singleton add

---
+++ single-ton.py
@@ -0,0 +1,19 @@
+class Singleton:
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
+
+
+
+
+
+a = Singleton()
+b = Singleton()
+
+print(id(a))
+print(id(b))
+print(a is b)
+print(a == b)
